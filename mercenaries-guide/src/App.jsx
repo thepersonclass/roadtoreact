29ad5d5deb56f01ee5950576e5d39dada9bcb917
Daniel Favano <thepersonclass@gmail.com>
2022-04-03 13:17:59 -0400

    Exercise: React Fragments
    In this exercise we wrapped the search component's returned JSX in a React fragment so we can have multiple top level pieces of HTML and not have the previous wrapping div as part of the markup.

--- mercenaries-guide/src/App.jsx
+++ mercenaries-guide/src/App.jsx
@@ -105,14 +105,14 @@ const Item = ({ title, url, author, num_comments, points }) => {
 }
 
 const Search = ({ search, onSearch }) => (
-    <div>
+    <>
       <label htmlFor="search">Search: </label>
       <input 
         id="search" 
         type="text" 
         value={search}
         onChange={onSearch} />  
-    </div>
+    </>
 );
 
 
