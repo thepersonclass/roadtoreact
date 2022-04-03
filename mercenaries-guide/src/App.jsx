a4c4f4dbe6151b9a9848942323906e99a8734468
Daniel Favano <thepersonclass@gmail.com>
2022-04-02 21:47:10 -0400

    Exercise: OnChange Events with React
    In this exercise we explored the React version of the HTML onChange event.

--- mercenaries-guide/src/App.jsx
+++ mercenaries-guide/src/App.jsx
@@ -76,11 +76,17 @@ const List = () => {
   );
 }
 
-const Search = () => (
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />  
+      <input id="search" type="text" onChange={handleChange} />  
     </div>
-)
+  );
+}
 
 export default App;
