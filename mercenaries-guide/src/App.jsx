2b1b1cc1aa3c022555e4ece42e04940d4102ff6b
Daniel Favano <thepersonclass@gmail.com>
2022-04-02 21:26:05 -0400

    Exercise: Component Hierarchies
    In this exercise we learned how to split up functionality between various components. This is typical when applications become more complex.

--- mercenaries-guide/src/App.jsx
+++ mercenaries-guide/src/App.jsx
@@ -1,59 +1,68 @@
 import './App.css';
 
-const list = [
-  {
-    title: 'Mercenaries Saga: Will of the White Lions',
-    url: 'https://www.nintendo.com/store/products/mercenaries-saga-chronicles-switch/',
-    publisher: 'Circle Ent.',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Mercenaries Saga 2: Order of the Silver Eagle',
-    url: 'https://www.nintendo.com/store/products/mercenaries-saga-chronicles-switch/',
-    publisher: 'Circle Ent.',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: 'Mercenaries Saga 3: Gray Wolves of War',
-    url: 'https://www.nintendo.com/store/products/mercenaries-saga-chronicles-switch/',
-    publisher: 'Circle Ent.',
-    num_comments: 2,
-    points: 5,
-    objectID: 2,
-  },
-  {
-    title: 'Mercenaries Blaze: Dawn of the Twin Dragons',
-    url: 'https://www.nintendo.com/store/products/mercenaries-blaze-dawn-of-the-twin-dragons-switch/?sid=164a9ed8cd472b0c3becf62dfeacf67e__ga',
-    publisher: 'Circle Ent.',
-    num_comments: 2,
-    points: 7,
-    objectID: 3,
-  },
-  {
-    title: 'Mercenaries Rebirth: Call of the Wild Lynx',
-    url: 'https://www.nintendo.com/store/products/mercenaries-rebirth-call-of-the-wild-lynx-switch/?sid=9e76add5afc193e4eeb6755d5ffe9def__ga',
-    publisher: 'Circle Ent.',
-    num_comments: 2,
-    points: 8,
-    objectID: 4,
-  }
-]
-
 function App() {
+
   return (
    <div>
-     <h1>Mercenaries Titles</h1>
+    <h1>Mercenaries Titles</h1>
 
-     <label htmlFor="search">Search: </label>
-     <input id="search" type="text" />
+    <Search />
 
      <hr />
 
-     <ul>
+     <List />
+
+   </div>
+  );
+}
+
+function List() {
+
+  const list = [
+    {
+      title: 'Mercenaries Saga: Will of the White Lions',
+      url: 'https://www.nintendo.com/store/products/mercenaries-saga-chronicles-switch/',
+      publisher: 'Circle Ent.',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Mercenaries Saga 2: Order of the Silver Eagle',
+      url: 'https://www.nintendo.com/store/products/mercenaries-saga-chronicles-switch/',
+      publisher: 'Circle Ent.',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+    {
+      title: 'Mercenaries Saga 3: Gray Wolves of War',
+      url: 'https://www.nintendo.com/store/products/mercenaries-saga-chronicles-switch/',
+      publisher: 'Circle Ent.',
+      num_comments: 2,
+      points: 5,
+      objectID: 2,
+    },
+    {
+      title: 'Mercenaries Blaze: Dawn of the Twin Dragons',
+      url: 'https://www.nintendo.com/store/products/mercenaries-blaze-dawn-of-the-twin-dragons-switch/?sid=164a9ed8cd472b0c3becf62dfeacf67e__ga',
+      publisher: 'Circle Ent.',
+      num_comments: 2,
+      points: 7,
+      objectID: 3,
+    },
+    {
+      title: 'Mercenaries Rebirth: Call of the Wild Lynx',
+      url: 'https://www.nintendo.com/store/products/mercenaries-rebirth-call-of-the-wild-lynx-switch/?sid=9e76add5afc193e4eeb6755d5ffe9def__ga',
+      publisher: 'Circle Ent.',
+      num_comments: 2,
+      points: 8,
+      objectID: 4,
+    }
+  ];
+  
+  return (
+    <ul>
        {list.map(function (item) {
          return (
           <li key={item.objectID}> 
@@ -66,9 +75,17 @@ function App() {
           </li>
          );
        })}
-     </ul>
-   </div>
+    </ul>
   );
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />  
+    </div>
+  )
 }
 
 export default App;
